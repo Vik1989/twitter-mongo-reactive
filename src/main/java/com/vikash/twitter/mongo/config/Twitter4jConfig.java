37fd50d3c4c0f29a5961cedba86b79661d7c8b71
Vikash Kumar <vvik10072@gmail.com>
2019-08-25 19:36:02 +0530

    Removing sensitive twitter ingo

--- src/main/java/com/vikash/twitter/mongo/config/Twitter4jConfig.java
+++ src/main/java/com/vikash/twitter/mongo/config/Twitter4jConfig.java
@@ -15,10 +15,7 @@ public class Twitter4jConfig {
 
         ConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
 
-        configurationBuilder.setOAuthAccessToken("1367546552-zo6T1I7jfU0wmuQQcL3zvuWkr7RKKKSl3mvt0ma")
-                .setOAuthAccessTokenSecret("05cEELivOoAWNnaZm5KI97hrD4NCuklLGLq5qjEmaiqGa")
-                .setOAuthConsumerKey("Amp5AWrQF2gETaEj5y5DQf614")
-                .setOAuthConsumerSecret("8Od8df4vbypYqcqYxheVSygWwQt31VSNcMFaqPlJCsqcUVbtzo");
+
 
         twitter4j.conf.Configuration config = configurationBuilder.build();
 
