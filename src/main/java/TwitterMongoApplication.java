c2615b1d2e2ce4d96cb78c51fc38f8e4880ad6db
Vikash Kumar <vvik10072@gmail.com>
2019-08-25 17:57:46 +0530

    First commit on new project

---
+++ src/main/java/TwitterMongoApplication.java
@@ -0,0 +1,11 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterMongoApplication.class, args);
+	}
+
+}
